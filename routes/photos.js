6e84f66c22a010d25afbbb0201ae79ba45ed5210
emmanuellabinjo <emmanuellabinjo@gmail.com>
2026-03-18 03:12:37 +0000

    Replace uuid with crypto's randomUUID

--- routes/photos.js
+++ routes/photos.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const multer = require("multer");
-const { v4: uuidv4 } = require("uuid");
+const { randomUUID: uuidv4 } = require("crypto");
 const { authenticate, requireRole } = require("../middleware/auth");
 const {
   createPhoto,
